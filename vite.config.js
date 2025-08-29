9dffc23d975e2d3c189625fe5eafe3cd00ec66d7
usmanamjadsmlab <usmanamjadsmlab@gmail.com>
2025-08-29 16:16:51 +0500

    fix vite config and index.html for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ECommerece/", // replace with your repository name
+  base: "/Ecommerce/", // 👈 repo ka exact naam, case-sensitive
 });
