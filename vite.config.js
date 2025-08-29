50c4c0f651359968c15d09901d7e557a96b3c64c
usmanamjadsmlab <usmanamjadsmlab@gmail.com>
2025-08-29 16:09:42 +0500

    icon changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ECommerece/", // replace with your repository name
+});
